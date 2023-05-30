8b2ba544215feff740763a56aaa455d44ad0e0f7
JoaoVitorFB <joao.batista@sptech.school>
2023-05-29 22:22:54 -0300

    esquema quiz finalizado

---
+++ js/quiz.js
@@ -0,0 +1,8 @@
+function quiz(perguntaAtual, perguntaAnterior){
+    // perguntas.style.display = "none";
+  
+    document.getElementById(perguntaAtual).style.display = "block";
+    document.getElementById(perguntaAnterior).style.display = "none";
+    // document.getElementById(pergunta).style.display = "block";
+    
+}
