d25fcf376d85bdaaa067fecd2296c8b852f004d2
JoaoVitorFB <joao.batista@sptech.school>
2023-05-30 21:47:04 -0300

    Tela de login e adicionar noticias finalizadas

--- js/quiz.js
+++ js/quiz.js
@@ -1,8 +1,5 @@
 function quiz(perguntaAtual, perguntaAnterior){
-    // perguntas.style.display = "none";
-  
+
     document.getElementById(perguntaAtual).style.display = "block";
     document.getElementById(perguntaAnterior).style.display = "none";
-    // document.getElementById(pergunta).style.display = "block";
-    
 }
